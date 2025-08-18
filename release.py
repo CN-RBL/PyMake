1c11ae97a806126a2b82cba841e5435a033d796f
CN-RBL <red.blue.light@qq.com>
2025-08-18 15:25:46 +0800

    1.1.5 release ready

---
+++ release.py
@@ -0,0 +1,26 @@
+import py7zr
+import zipfile
+import os
+import shutil
+
+try:shutil.rmtree(".release")
+except:pass
+os.mkdir(".release")
+
+if os.path.exists("pymake.7z"):
+    os.remove("pymake.7z")
+
+if os.path.exists("pymake.zip"):
+    os.remove("pymake.zip")
+
+os.system(".venv\\Scripts\\python.exe main.py --load-config config.json")
+os.rename(".release\\main.exe", ".release\\pymake.exe")
+shutil.copytree("lang", ".release\\lang\\")
+
+with py7zr.SevenZipFile("pymake.7z", "w", password="pymake") as release_7z:
+    release_7z.writeall(".release/")
+
+with zipfile.ZipFile("pymake.zip", "r") as release_zip:
+    release_zip.extractall(".release/")
+
+shutil.rmtree(".release")
